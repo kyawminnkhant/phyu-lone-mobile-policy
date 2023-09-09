4654854f0a87a90cddc65ac7eae2826657ce0341
Kyawminn Khant <mr.kyawminnkhant@gmail.com>
2023-09-09 19:08:38 +0630

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Phyu Lone Mobile</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
